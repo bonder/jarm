14bdf4f022be05cd6214a0d95e502c81da5d6241
Rob <rob.s.brit@gmail.com>
2010-02-14 01:51:31 -0500

    Added licencing info.

--- helpers.js
+++ helpers.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2010 Rob Britton
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 var distSq = function(pos1, pos2){
   return (pos1.top - pos2.top) * (pos1.top - pos2.top) +
     (pos1.left - pos2.left) * (pos1.left - pos2.left);
